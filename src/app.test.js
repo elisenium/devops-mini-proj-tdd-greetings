8ad9c445901a6f2f700d32f64b94f2021b4d6821
elisenium <elise.grelaud@student.vinci.be>
2023-11-04 23:43:59 +0100

    feat(greet): prettier:fix

--- src/app.test.js
+++ src/app.test.js
@@ -1,5 +1,5 @@
 const greet = require('./app.js');
 
 test('greet with given name in parameter', () => {
-    expect(greet("Bob")).toBe("Hello, Bob.");
+    expect(greet('Bob')).toBe('Hello, Bob.');
 });
