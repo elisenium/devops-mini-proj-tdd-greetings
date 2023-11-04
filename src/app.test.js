2c16a44c042a0818dcd95b66db66784c254f515f
elisenium <elise.grelaud@student.vinci.be>
2023-11-04 23:38:19 +0100

    feat(default-value): prettier:fix cmd

--- src/app.test.js
+++ src/app.test.js
@@ -1,17 +1,17 @@
 const greet = require('./app.js');
 
 test('greet with given name in parameter', () => {
-    expect(greet("Bob")).toBe("Hello, Bob.");
+    expect(greet('Bob')).toBe('Hello, Bob.');
 });
 
 test('greet with an undefined value', () => {
-    expect(greet(undefined)).toBe("Hello, my friend.");
+    expect(greet(undefined)).toBe('Hello, my friend.');
 });
 
 test('greet with no entered value', () => {
-    expect(greet()).toBe("Hello, my friend.");
+    expect(greet()).toBe('Hello, my friend.');
 });
 
 test('greet with a null value', () => {
-    expect(greet(null)).toBe("Hello, my friend.");
+    expect(greet(null)).toBe('Hello, my friend.');
 });
