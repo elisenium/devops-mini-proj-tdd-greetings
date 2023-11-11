0d07965b339620b9d3c72ebcecf52981f3deab90
didia <didia.josefechado@student.vinci.be>
2023-11-11 14:18:42 +0100

    feat(manage-mix): creation new branch + new test

--- src/app.test.js
+++ src/app.test.js
@@ -7,3 +7,7 @@ test('greet with given regular name in parameter', () => {
 test('greet with given uppercase name in parameter', () => {
     expect(greet('JERRY')).toBe('HELLO, JERRY!');
 });
+
+test('greet with given uppercase name in parameter', () => {
+    expect(greet(["Amy", "BRIAN", "Charlotte"])).toBe('Hello, Amy and Charlotte. AND HELLO BRIAN !');
+});
