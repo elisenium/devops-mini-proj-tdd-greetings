00b3f8ca52bf0f617bb6f0574314ca5a0a068e22
elisenium <elise.grelaud@student.vinci.be>
2023-11-13 16:25:08 +0100

    feat(default-value): prettier:fix

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-function greet(names){
-    if (names.length==2){
-        return `Hello, ${names[0]} and ${names[1]}.`;}
+function greet(names) {
+    if (names.length == 2) {
+        return `Hello, ${names[0]} and ${names[1]}.`;
+    }
     return moreThanTwo(names);
-};
+}
 
-function moreThanTwo(names){
-     return `Hello, ${names[0]}, ${names[1]} and ${names[2]}.`;
-};
+function moreThanTwo(names) {
+    return `Hello, ${names[0]}, ${names[1]} and ${names[2]}.`;
+}
 
 module.exports = greet;
